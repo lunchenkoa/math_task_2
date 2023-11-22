10fdfae87df008e6ca955c5829f2425b8f8bc893
vvickia <victoria.kobozeva@gmail.com>
2023-11-22 16:10:40 +0300

    correction of the struct of cons

--- headers/variables.hpp
+++ headers/variables.hpp
@@ -21,14 +21,27 @@ struct primitive_variables
 
     static int N = 40;                         // number of grid cells (40, 80, 160, 320)
     static double C = 0.3;                     // Courant number       (0.3, 0.6, 0.9)
-    const double x_L = -0.5;            // coordinate borders
+    const double x_L = -0.5;                   // coordinate borders
     const double x_R = 0.5;
     static double dx = (x_R - x_L) / N;        // delta x (step)
 
 struct conservative_variables
 {
-    double ** u = create_array(N, 3);    // u = {ρ, ρ*v, E}, where E = p/(γ-1)+ρ*v*v/2
-    double ** F = create_array(N, 3);    // F = {ρ*v, ρ*v*v+p, p*v/(γ-1)+ρ*v*v*v/2+p*v}
+    double** u;    // u = {ρ, ρ*v, E}, where E = p/(γ-1)+ρ*v*v/2
+    double** F;    // F = {ρ*v, ρ*v*v+p, p*v/(γ-1)+ρ*v*v*v/2+p*v}
+
+    // conservative_variables() : u(create_array(N, 3)), F(create_array(N, 3)) {}
+
+    // ~conservative_variables()
+    // {
+    //     for (int i = 0; i < N; ++i)
+    //     {
+    //         delete[] u[i];
+    //         delete[] F[i];
+    //     }
+    //     delete[] u;
+    //     delete[] F;
+    // }
 };
 
 #endif
